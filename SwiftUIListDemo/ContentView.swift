dbfaf7249798f8dc3941802248cf0661a94a37e3
RajendranEshwaran <rajendranmca2010@gmail.com>
2020-10-10 10:40:03 -0400

    Initial Commit

---
+++ SwiftUIListDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIListDemo
+//
+//  Created by gomathi saminathan on 10/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIListDemo/SwiftUIListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListDemoApp.swift
+//  SwiftUIListDemo
+//
+//  Created by gomathi saminathan on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
